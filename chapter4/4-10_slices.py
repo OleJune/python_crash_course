6605b7bc55c2962b1106062a1af1245ce86c7d4c
Ole June <khmyzok@gmail.com>
2023-10-22 21:15:16 +0300

    Add 13 exercises from Chapter4

---
+++ chapter4/4-10_slices.py
@@ -0,0 +1,11 @@
+animals = ['panda', 'koala', 'dog', 'cat', 'bird']
+
+print('The first three items in the list are:')
+#print(animals[0:3])
+print(animals[:3])
+
+print('\nThree items from the middle of the list are:')
+print(animals[1:4])
+
+print('\nThe last three items in the list are:')
+print(animals[-3:])
---
+++ chapter4/4-13_buffet.py
@@ -0,0 +1,15 @@
+print('Original menu includes:')
+buffet_offers = ('steak', 'salad', 'pasta', 'soup', 'dessert')
+for food in buffet_offers:
+	print(food)
+
+# Try to modify one of the items:
+
+#buffet_offers[0] = 'juice'
+#print(buffet_offers)
+
+print('\nRevised menu includes:')
+
+buffet_offers = ('chiken wings', 'salad', 'rice', 'soup', 'dessert')
+for food in buffet_offers:
+	print(food)
